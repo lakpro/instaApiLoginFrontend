ed4acd00710a815118fa2fb02ba8c6ccc2e45091
Lakshay Setia <lakshaysetia123@gmail.com>
2025-04-10 04:29:47 +0530

    before publish

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+import { copyFileSync } from "fs";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  build: {
+    rollupOptions: {
+      // optional config
+    },
+    outDir: "dist",
+  },
+  // Copy _redirects after build
+  closeBundle: () => {
+    copyFileSync(resolve("public/_redirects"), resolve("dist/_redirects"));
+  },
 });
